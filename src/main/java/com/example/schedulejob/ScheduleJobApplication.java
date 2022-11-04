8b0858146def3212b9449d118b7b88cf7d444b0c
caner akin <caner.akin@secomtr.com>
2022-11-04 16:04:44 +0300

    "v2"

--- src/main/java/com/example/schedulejob/ScheduleJobApplication.java
+++ src/main/java/com/example/schedulejob/ScheduleJobApplication.java
@@ -3,11 +3,12 @@ package com.example.schedulejob;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class ScheduleJobApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ScheduleJobApplication.class, args);
     }
-
 }
