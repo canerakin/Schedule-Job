7753a129c2973ef662434af4596726326d8cde18
caner akin <caner.akin@secomtr.com>
2022-11-04 15:52:09 +0300

    v1

---
+++ src/main/java/com/example/schedulejob/ScheduleJobApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulejob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleJobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schedulejob/ScheduleJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulejob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
