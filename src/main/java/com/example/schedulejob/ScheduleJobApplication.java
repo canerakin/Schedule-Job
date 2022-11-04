f3d2dcaa2aab4e09ee7b20ccbe0052237ffd60ff
caner akin <caner.akin@secomtr.com>
2022-11-04 16:45:49 +0300

    "v3"

--- src/main/java/com/example/schedulejob/ScheduleJobApplication.java
+++ src/main/java/com/example/schedulejob/ScheduleJobApplication.java
@@ -2,10 +2,12 @@ package com.example.schedulejob;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
+@EnableScheduling
 public class ScheduleJobApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/schedulejob/config/Scheduler.java
@@ -0,0 +1,7 @@
+package com.example.schedulejob.config;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+public class Scheduler {
+
+}
